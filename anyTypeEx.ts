d07d4fa64a2c34f174e404108c10a2526c025456
bulankaranuja <148444539+bulankaranuja@users.noreply.github.com>
2023-12-19 09:31:58 +0530

    AllTypeFile Uploaded
    Working on All types in typescript

---
+++ anyTypeEx.ts
@@ -0,0 +1,10 @@
+let firstName:string = "Anuja";
+let len = firstName.length;
+
+console.log(len);
+
+const sequence:number[] = [45,4,9,16,25];
+
+sequence.forEach((number) => {
+    console.log(number);
+})
